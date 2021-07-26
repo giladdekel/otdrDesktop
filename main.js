376ba294aad6f1ff5ce96fb93334478175b051c1
Shmulik Hemo <Shmulikhe@rs-traffic.com>
2021-07-26 12:00:34 +0300

    f

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-
-const { BrowserWindow, app, ipcMain, Notification } = require('electron');
+const { BrowserWindow, app, ipcMain, Notification } = require("electron");
 const notifier = require("node-notifier");
-const path = require('path');
+const path = require("path");
 
 const isDev = !app.isPackaged;
 
 //const axios = require("axios");
 
 const log4js = require("log4js");
-
-
-
 
 const socketIOClient = require("socket.io-client");
 
@@ -33,75 +29,67 @@ logger.trace("\n\n\n\n\n\n\n");
 
 logger.trace("Entering OTDR testing");
 
-
-
-
-sk.on("onFailure", (data) => {
-  console.log("onFailure");
-  // console.log("data :", data);
-  console.log("timeFailed: ", data.time);
-
-  console.log("lengthFailed: ", data.length);
-  ////logger file
-
-  logger.warn(`length of failure: ${data.length}`);
-
-  /////alert in the desktop:
-if(Number(data.length)===0){
-  notifier.notify({
-    title: "OTDR Faliure",
-    message: `no fiber`,
-    icon: path.join(__dirname, 'images/failure.png'), // Absolute path (doesn't work on balloons)
-  });
-}
-else{
-   notifier.notify({
-    title: "OTDR Faliure",
-    message: `fiber break at : ${data.length}`,
-    icon: path.join(__dirname, 'images/failure.png'), // Absolute path (doesn't work on balloons)
-  }); 
-}
-
-
-  notifier.on('click', function (notifierObject, options, event) {
-    // Triggers if `wait: true` and user clicks notification
-
-    // console.log("fail click")
-    sk.emit("muteVolume");
-  
-    
-  });
-  
-});
-
-
-
-
-sk.on("onSuccess", (data) => {
-  console.log("onSuccess");
+sk.on("onFiberChange", (data) => {
+  console.log("onFiberChange");
   // console.log("data :", data);
 
-  console.log("timeSuccess: ", data.time);
+  if (data.type === "success") {
+    // console.log("data :", data);
 
-  console.log("lengtSuccess: ", data.length);
-  ////logger file
+    console.log("timeSuccess: ", data.time);
 
-  logger.info(`length of success: ${data.length}`);
+    console.log("lengtSuccess: ", data.length);
+    ////logger file
 
-  /////alert in the desktop:
+    logger.info(`length of success: ${data.length}`);
 
-  notifier.notify({
-    title: "OTDR Success",
-    message: `Length of fiber: ${data.length} meter`,
+    /////alert in the desktop:
+
+    notifier.notify({
+      title: "OTDR Success",
+      message: `Length of fiber: ${data.length} meter`,
       wait: true, // Wait for User Action against Notification or times out. Same as timeout = 5 seconds
-      icon: path.join(__dirname, 'images/success.png'), // Absolute path (doesn't work on balloons)
+      icon: path.join(__dirname, "images/success.png"), // Absolute path (doesn't work on balloons)
+    });
+  } else {
+    if (data.type === "noFiber") {
+      // console.log("data :", data);
+      console.log("timeFailed: ", data.time);
 
-  });
+      console.log("no fiber");
+      ////logger file
 
+      logger.warn(`no fiber`);
+
+      notifier.notify({
+        title: "OTDR Faliure",
+        message: `No fiber`,
+        icon: path.join(__dirname, "images/failure.png"), // Absolute path (doesn't work on balloons)
+      });
+    } else if (data.type === "faliure") {
+      // console.log("data :", data);
+      console.log("timeFailed: ", data.time);
+
+      console.log("lengthFailed: ", data.length);
+      ////logger file
+
+      logger.warn(`Fiber break at: ${data.length} meter`);
+
+      notifier.notify({
+        title: "OTDR Faliure",
+        message: `Fiber break at : ${data.length} meter`,
+        icon: path.join(__dirname, "images/failure.png"), // Absolute path (doesn't work on balloons)
+      });
+    }
+
+    notifier.on("click", function (notifierObject, options, event) {
+      // Triggers if `wait: true` and user clicks notification
+
+      // console.log("fail click")
+      sk.emit("muteVolume");
+    });
+  }
 });
-
-
-
 
 sk.on("onError", (data) => {
   console.log("onError");
@@ -109,15 +97,80 @@ sk.on("onError", (data) => {
 
   ////logger file
 
-  logger.error(`error in request: ${data}`);
+  logger.error(`error in request: ${data.message}`);
 
+  notifier.notify({
+    title: "OTDR Faliure",
+    message: `Error in request: ${data.message}`,
+    icon: path.join(__dirname, "images/failure.png"), // Absolute path (doesn't work on balloons)
+  });
 
+  // notifier.on("click", function (notifierObject, options, event) {
+  //   // Triggers if `wait: true` and user clicks notification
+
+  //   // console.log("fail click")
+  //   sk.emit("muteVolume");
+  // });
 });
 
+// sk.on("onFailure", (data) => {
+//   console.log("onFailure");
+//   // console.log("data :", data);
+//   console.log("timeFailed: ", data.time);
 
+//   console.log("lengthFailed: ", data.length);
+//   ////logger file
 
+//   logger.warn(`length of failure: ${data.length}`);
 
+//   /////alert in the desktop:
+// if(Number(data.length)===0){
+//   notifier.notify({
+//     title: "OTDR Faliure",
+//     message: `no fiber`,
+//     icon: path.join(__dirname, 'images/failure.png'), // Absolute path (doesn't work on balloons)
+//   });
+// }
+// else{
+//    notifier.notify({
+//     title: "OTDR Faliure",
+//     message: `fiber break at : ${data.length}`,
+//     icon: path.join(__dirname, 'images/failure.png'), // Absolute path (doesn't work on balloons)
+//   });
+// }
 
+//   notifier.on('click', function (notifierObject, options, event) {
+//     // Triggers if `wait: true` and user clicks notification
+
+//     // console.log("fail click")
+//     sk.emit("muteVolume");
+
+//   });
+
+// });
+
+// sk.on("onSuccess", (data) => {
+//   console.log("onSuccess");
+//   // console.log("data :", data);
+
+//   console.log("timeSuccess: ", data.time);
+
+//   console.log("lengtSuccess: ", data.length);
+//   ////logger file
+
+//   logger.info(`length of success: ${data.length}`);
+
+//   /////alert in the desktop:
+
+//   notifier.notify({
+//     title: "OTDR Success",
+//     message: `Length of fiber: ${data.length} meter`,
+//       wait: true, // Wait for User Action against Notification or times out. Same as timeout = 5 seconds
+//       icon: path.join(__dirname, 'images/success.png'), // Absolute path (doesn't work on balloons)
+
+//   });
+
+// });
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -128,22 +181,22 @@ function createWindow() {
       nodeIntegration: false,
       worldSafeExecuteJavaScript: true,
       contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-  win.loadFile('index.html');
+  win.loadFile("index.html");
 }
 
 if (isDev) {
-  require('electron-reload')(__dirname, {
-    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-  })
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+  });
 }
 
-ipcMain.on('notify', (_, message) => {
-  new Notification({title: 'OTDR Notifiation', body: message}).show();
-})
+ipcMain.on("notify", (_, message) => {
+  new Notification({ title: "OTDR Notifiation", body: message }).show();
+});
 
 // String
 // notifier.notify('Message');
@@ -154,11 +207,8 @@ ipcMain.on('notify', (_, message) => {
 //   message: 'Hellooooo, there!'
 // });
 
-
 // notifier.on("click", function (notifierObject, options, event) {
 // console.log('notifierObject :', notifierObject);
 //   // Triggers if `wait: true` and user clicks notification
 // });
-app.whenReady().then(createWindow)
-
-
+app.whenReady().then(createWindow);
--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 import Alert from "@material-ui/lab/Alert";
 
 import AudioPlayer from "react-h5-audio-player";
@@ -10,20 +10,19 @@ import "react-h5-audio-player/lib/styles.css";
 // import { notifier } from 'node-notifier';
 import socketIOClient from "socket.io-client";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faVolumeMute, faVolumeUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeMute, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
 
-const ENDPOINT ="http://192.168.1.218:5000";
-
+const ENDPOINT = "http://192.168.1.218:5000";
 
 export default function App() {
-
-
-
   const [socket, setSocket] = useState(null);
 
   const [lengthFaliure, setLengthFaliure] = useState(false);
   const [lengthSuccess, setLengthSuccess] = useState(false);
+  const [noFiber, setNoFiber] = useState(false);
+  const [errorReq, setErrorReq] = useState(false);
+
   const [alert, setAlert] = useState(false);
 
   useEffect(() => {
@@ -40,33 +39,72 @@ export default function App() {
 
       // console.log("room.roomId :", roomId);
 
+      // sk.on("onFiberChange", (data) => {
+      //   console.log("onFiberChange");
+      //   console.log("data :", data);
+      //   if (data.type === "faliure") {
+      //     setLengthFaliure(data.length);
+      //     setLengthSuccess(false);
+      //     setAlert(true);
+      //   }
+
+      //   else if (data.type === "success") {
+      //     setLengthFaliure(false);
+      //     setLengthSuccess(data.length);
+      //     setAlert(false);
+      //   }
+      // });
+
       sk.on("onFiberChange", (data) => {
         console.log("onFiberChange");
-        console.log("data :", data);
-        if (data.type === "faliure") {
-          setLengthFaliure(data.length);
-          setLengthSuccess(false);
-          setAlert(true);
+        // console.log("data :", data);
 
-        } 
-        
-        else if (data.type === "success") {
+        if (data.type === "success") {
+          // console.log("data :", data);
           setLengthFaliure(false);
           setLengthSuccess(data.length);
+          setNoFiber(false);
+          setErrorReq(false);
           setAlert(false);
+        } else {
+          if (data.type === "noFiber") {
+            setLengthFaliure(false);
+            setLengthSuccess(false);
+            setNoFiber(true);
+            setErrorReq(false);
+
+            setAlert(true);
+          } else if (data.type === "faliure") {
+            setLengthFaliure(data.length);
+            setLengthSuccess(false);
+            setNoFiber(false);
+            setErrorReq(false);
+
+            setAlert(true);
+          }
         }
       });
 
-      sk.on("onVolumeMute", () => {
-        console.log("onVolumeMute");
-        
+      sk.on("onError", (data) => {
+        console.log("onError");
+
+        setLengthFaliure(false);
+        setLengthSuccess(false);
+        setNoFiber(false);
+        setErrorReq(data.message);
+
         setAlert(false);
         // setData(data);
       });
 
-    }
-  }, [socket, lengthFaliure,lengthSuccess]);
+      sk.on("onVolumeMute", () => {
+        console.log("onVolumeMute");
 
+        setAlert(false);
+        // setData(data);
+      });
+    }
+  }, [socket]);
 
   const useStyles = makeStyles((theme) => ({
     root: {
@@ -79,23 +117,25 @@ export default function App() {
 
   const classes = useStyles();
 
-
   return (
-    <>          
+    <>
+      <div
+        style={{
+          backgroundImage: `url(images/fiber1.jpg)`,
+          height: "640px",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          position: "relative",
+        }}
+      >
+        <React.Fragment>
+          <CssBaseline />
+          <Container maxWidth="sm">
+            <h1 className="rainbow-text">OTDR Test </h1>
 
-<div  style={{ backgroundImage: `url(images/fiber1.jpg)`, height:"640px", backgroundPosition: "center",
-  backgroundRepeat:"no-repeat",
-  backgroundSize: "cover",
-  position: "relative" }}>  
-
-   <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="sm"  >        
-        <h1 className="rainbow-text">OTDR Test </h1>
-
-        <Typography component="div" >
-
-      {/* <button
+            <Typography component="div">
+              {/* <button
         onClick={() => {
           // electron.notificationApi.sendNotification("My custom notification!");
           setLengthFaliure((prev) => !prev);
@@ -105,51 +145,162 @@ export default function App() {
         Notify
       </button> */}
 
-      <div className={classes.root}>
-        {lengthFaliure && (
-          <>
+              <div className={classes.root}>
+                {errorReq && (
+                  <>
+                    <Alert className="alert-fail" severity="error">
+                      OTDR Error in request - {errorReq}!
+                      {alert ? (
+                        <>
+                          {" "}
+                          <FontAwesomeIcon
+                            className="vol-icon"
+                            icon={faVolumeUp}
+                            size="2x"
+                            onClick={() => setAlert(false)}
+                          />
+                        </>
+                      ) : (
+                        <>
+                          {" "}
+                          <FontAwesomeIcon
+                            className="vol-icon"
+                            icon={faVolumeMute}
+                            size="2x"
+                            onClick={() => setAlert(true)}
+                          />
+                        </>
+                      )}
+                    </Alert>
 
-            <Alert className="alert-fail" severity="error" >
-              OTDR Faliure, fiber break at {lengthFaliure} meter!
-              {!alert ? <> <FontAwesomeIcon className="vol-icon" icon={faVolumeMute}  size="2x" onClick={() => setAlert((prev) => !prev)} /></>:<> <FontAwesomeIcon className="vol-icon" icon={faVolumeUp}  size="2x" onClick={() => setAlert((prev) => !prev)} /></>}     
-             
-            </Alert>
-            {alert && (
-              <>
-                {/* {electron.notificationApi.sendNotification("OTDR Fail at!")}; */}
-                <div className="alert" style={{ display: "none" }}>
-                  <AudioPlayer
-                    autoPlay
-                    loop
-                    showJumpControls={false}
-                    showDownloadProgress={false}
-                    showFilledProgress={false}
-                    hasDefaultKeyBindings={false}
-                    src="a1.mp3"
-                    onPlay={(e) => console.log("onPlay")}
-                    // other props here
-                  />
-                </div>
-              </>
-            )}
+                    {alert && (
+                      <>
+                        {/* {electron.notificationApi.sendNotification("OTDR Fail at!")}; */}
+                        <div className="alert" style={{ display: "none" }}>
+                          <AudioPlayer
+                            autoPlay
+                            loop
+                            showJumpControls={false}
+                            showDownloadProgress={false}
+                            showFilledProgress={false}
+                            hasDefaultKeyBindings={false}
+                            src="a1.mp3"
+                            onPlay={(e) => console.log("onPlay")}
+                            // other props here
+                          />
+                        </div>
+                      </>
+                    )}
+                  </>
+                )}
 
+                {noFiber && (
+                  <>
+                    <Alert className="alert-fail" severity="error">
+                      OTDR Faliure - no fiber!
+                      {alert ? (
+                        <>
+                          {" "}
+                          <FontAwesomeIcon
+                            className="vol-icon"
+                            icon={faVolumeUp}
+                            size="2x"
+                            onClick={() => setAlert(false)}
+                          />
+                        </>
+                      ) : (
+                        <>
+                          {" "}
+                          <FontAwesomeIcon
+                            className="vol-icon"
+                            icon={faVolumeMute}
+                            size="2x"
+                            onClick={() => setAlert(true)}
+                          />
+                        </>
+                      )}
+                    </Alert>
 
-          </>
-        )}
-        {/* <Alert severity="warning">This is a warning alert — check it out!</Alert>
+                    {alert && (
+                      <>
+                        {/* {electron.notificationApi.sendNotification("OTDR Fail at!")}; */}
+                        <div className="alert" style={{ display: "none" }}>
+                          <AudioPlayer
+                            autoPlay
+                            loop
+                            showJumpControls={false}
+                            showDownloadProgress={false}
+                            showFilledProgress={false}
+                            hasDefaultKeyBindings={false}
+                            src="a1.mp3"
+                            onPlay={(e) => console.log("onPlay")}
+                            // other props here
+                          />
+                        </div>
+                      </>
+                    )}
+                  </>
+                )}
+
+                {lengthFaliure && (
+                  <>
+                    <Alert className="alert-fail" severity="error">
+                      OTDR Faliure - fiber break at {lengthFaliure} meter!
+                      {alert ? (
+                        <>
+                          {" "}
+                          <FontAwesomeIcon
+                            className="vol-icon"
+                            icon={faVolumeUp}
+                            size="2x"
+                            onClick={() => setAlert(false)}
+                          />
+                        </>
+                      ) : (
+                        <>
+                          {" "}
+                          <FontAwesomeIcon
+                            className="vol-icon"
+                            icon={faVolumeMute}
+                            size="2x"
+                            onClick={() => setAlert(true)}
+                          />
+                        </>
+                      )}
+                    </Alert>
+
+                    {alert && (
+                      <>
+                        {/* {electron.notificationApi.sendNotification("OTDR Fail at!")}; */}
+                        <div className="alert" style={{ display: "none" }}>
+                          <AudioPlayer
+                            autoPlay
+                            loop
+                            showJumpControls={false}
+                            showDownloadProgress={false}
+                            showFilledProgress={false}
+                            hasDefaultKeyBindings={false}
+                            src="a1.mp3"
+                            onPlay={(e) => console.log("onPlay")}
+                            // other props here
+                          />
+                        </div>
+                      </>
+                    )}
+                  </>
+                )}
+                {/* <Alert severity="warning">This is a warning alert — check it out!</Alert>
       <Alert severity="info">This is an info alert — check it out!</Alert> */}
-        {lengthSuccess && (
-          <Alert severity="success">
-            OTDR Success — fiber length :{lengthSuccess} meter
-          </Alert>
-        )}
-      </div>
-  
+                {lengthSuccess && (
+                  <Alert severity="success">
+                    OTDR Success — fiber length: {lengthSuccess} meter
+                  </Alert>
+                )}
+              </div>
             </Typography>
-      </Container>
-    </React.Fragment>
-
-  </div>
+          </Container>
+        </React.Fragment>
+      </div>
     </>
   );
 }
